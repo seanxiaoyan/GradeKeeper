51aee22dcba2edc48ef54b64f9dad35a70ba20fa
Xiaoyan Xu <xuxiaoyansean@gmail.com>
2019-09-17 02:01:47 -0600

    In the class addCourseCommand:
    fixed a bug that duplicate course (here, duplicate means course with the same name) would be allowed to add in the system.
    
    Also, changed the idea to check the duplicate course
    (Only check the name, by having a list to store all the names of the courses that are in the system, then check if the name of the course that is trying to be added is in the list).
    
    Now, duplicate course would not be allowed to added in the system, an error message would be given if trying to add a course with the name that is already in the system.

--- src/commands/addCourseCommand.java
+++ src/commands/addCourseCommand.java
@@ -3,14 +3,37 @@ import entities.Course;
 import containers.courseTree;
 import lib280.tree.OrderedSimpleTree280;
 import lib280.exception.*;
-
+import lib280.list.*;
 public class addCourseCommand extends commandStatus{
     /**
      * private boolean field, used to check if the course already exist.
      */
     private boolean isDuplicate=false;
 
+    /**
+     *   private field
+     *   a list to store all the current courses name, use it
+     *   to check duplicate name when adding course
+     */
+    private LinkedList280<String> nameList = new LinkedList280<String>();
 
+    /**
+     * tree traversal to get all the name of the courses that are currently stored in the tree
+     * @param T a binary search tree which stores Course objects
+     */
+    public void traversal(OrderedSimpleTree280<Course> T) {
+
+        if(!T.isEmpty()){ // check if the tree is empty first.
+            if(!T.rootRightSubtree().isEmpty()){
+                traversal(T.rootRightSubtree());
+            }
+
+            nameList.insert(T.rootItem().getName());
+
+            if(!T.rootLeftSubtree().isEmpty()){
+                traversal(T.rootLeftSubtree());}
+             }
+    }
     /**
      * Method addCourse: Create a course with its name. grade and credit units. Then add the course into the system.
      * @param name: name of course
@@ -18,27 +41,25 @@ public class addCourseCommand extends commandStatus{
      * @param credit: credit of course
      */
     public void addCourse (String name,int grade,int credit){
-        try {
-        Course c = new Course(name); // create a new course object
-        c.setGrade(grade);
-        c.setCredit(credit);
+        //Check the name of the course first. If the name already exists, means the course is already in the system
+        traversal(courseTree.tree());
 
-        //since .has() return true as long as the grade is the same, we need double check
-        if(courseTree.tree().has(c)){//check if there is a class with the same grade\
-            courseTree.tree().search(c);//move the cursor to that item position
-            if(courseTree.tree().item().getName().equals(name)){// if the class' name is also the same
-                successful = false;// false to add course, command was not successful
-                errorMessage = "Cannot add this course: "+name+" ,since it is already in the system";
+        if(nameList.has(name)){
+
+            successful = false;// false to add course, command was not successful
+            errorMessage = "Cannot add this course: "+name+" ,since it is already in the system";
+        }
+        else{
+            try {
+                Course c = new Course(name); // create a new course object
+                c.setGrade(grade);
+                c.setCredit(credit);
+                courseTree.tree().insert(c);//insert the course into the tree
+                successful = true; // command was successful
             }
-            courseTree.tree().insert(c);//insert the course into the tree
-            successful=true; // command was successful
+            catch (InvalidArgument280Exception e){successful=false; // command was not successful
+            errorMessage=e.getMessage();}
         }
-        else{// course is not yet in the system
-        courseTree.tree().insert(c);//insert the course into the tree
-        successful=true;} // command was successful
-        }
-        catch (InvalidArgument280Exception e){successful=false; // command was not successful
-        errorMessage=e.getMessage();}
     }
 
 }
